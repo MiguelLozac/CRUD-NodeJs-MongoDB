dd966b2630ac47afb9f640fb599edd9c722fcadf
MiguelLozac <miguellozaac@gmail.com>
2021-11-01 17:45:05 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CRUD-NodeJs-MongoDB
 practice web app, this app is about creating, editing and deleting tasks, using MongoDB, NodeJs, Bootstrap 5 &amp; Express  
+
+App listens to localhost://3000
+
+MongoDB is named: crud-mongo
---
+++ src/app.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+ 
+const app = express();
+
+// connecting to db
+mongoose.connect('mongodb://localhost/crud-mongo')
+    .then(db => console.log('db connected'))
+    .catch(err => console.log(err));
+
+// importing routes
+
+const indexRoutes = require('./routes/index');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use('/', indexRoutes);
+
+
+// starting the server 
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
+
